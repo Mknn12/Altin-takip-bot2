0a908f2a49175b3c1d6814ffd41f27d0694967bf
Mknn12 <kaankaan_2012@hotmail.com>
2025-07-17 12:31:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -484,9 +484,10 @@ def home():
         logger.error(f"❌ Dashboard hatası: {e}")
         return f"⚠️ Dashboard hatası: {str(e)}"
 if __name__ == "__main__":
+    init_db()  # Veritabanı ve tablo oluşturulsun
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-init_db()
+
 
 
 
